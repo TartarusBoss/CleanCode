093119fede82baf322c8febb236bb0b96ad82711
Matt <116335873+TartarusBoss@users.noreply.github.com>
2023-09-26 22:45:42 -0500

    Add files via upload

---
+++ cleancode/Usuario.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cleancode;
+
+/**
+ *
+ * @author Matt
+ */
+public class Usuario {
+    
+    public boolean iniciarSesion(){
+        
+    }
+    public void cerrarSesion(){
+        
+    }    
+    public void crearGrupoAhorro(){
+        
+    }    
+    public void solicitarPrestamo(){
+        
+    }    
+    public void realizarTransaccion(){
+        
+    }
+    public void verHistorial(){
+        
+    }
+}
