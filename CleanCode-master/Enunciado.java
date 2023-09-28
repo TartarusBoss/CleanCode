91ad4bb0b0274d1673713559eb7ed904445e7c7c
Matt <116335873+TartarusBoss@users.noreply.github.com>
2023-09-28 11:55:36 -0500

    Add files via upload

---
+++ CleanCode-master/Enunciado.java
@@ -0,0 +1,37 @@
+El UdeMBank está promoviendo un servicio de ahorro y préstamo comunitario y los contrata
+para desarrollar su aplicación. La aplicación debe cumplir con los siguientes requisitos:
+● Un usuario debe estar registrado y con sesión iniciada en UdeMBank para hacer uso
+de la aplicación ✓
+● Cada usuario registrado (usuario en el resto del documento) debe tener una cuenta
+de ahorros única en UdeMBank ✓
+○ Esta cuenta inicia con un valor $X inicial ✓
+● Un usuario registrado puede crear un grupo de ahorro donde puede invitar a máximo
+dos usuarios registrados diferentes ✓
+● Un usuario puede estar máximo en tres grupos de ahorro diferentes ✓ 
+● Un usuario puede ingresar capital ilimitado a la cuenta de ahorro del grupo ✓  
+● Un usuario podrá solicitar un préstamo de su grupo de ahorro siempre y cuando
+cumpla con las siguientes condiciones:
+○ El préstamo no supera el monto total invertido por el usuario ✓
+○ El plazo de pago no es inferior a dos meses ✓ 
+○ El interés será de 3% mensual ✓
+● El usuario podrá solicitar un préstamo de otro grupo de ahorro siempre y cuando otro
+usuario de uno de sus grupos de ahorro esté en ese grupo de ahorro
+○ Si pide prestado de este grupo, el interés será del 5% mensual
+● El usuario pagará por mensualidades. Cuando decida pagar, el sistema le deberá
+mostrar cuánto debe pagar según su deuda y el número de meses a pagar
+● Los usuarios de un grupo de ahorro podrán disolver un grupo de ahorro. Cuando lo
+hacen, el banco se queda con una comisión del 5% y el restante, se divide con base
+en el porcentaje de ahorro de cada usuario y se transfiere a sus cuentas personales.
+● Cada usuario podrá ver el histórico de sus movimientos de cuenta de ahorro y
+aportes al grupo de ahorro
+○ Esta vista muestra, movimiento a movimiento: fecha, hora, saldo actual
+cuenta ahorros, tipo de movimiento (consignación a cuenta de ahorro,
+transferencia a grupo de ahorro, préstamo grupo de ahorro), monto, saldo
+final.
+● Por cada transacción que se hace en la app, UdeMBank recibe el 0.1% de comisión
+● El equipo de fidelización del banco premia al equipo de ahorro con más ganancias
+En este proceso, se evalúa cuál equipo de ahorro ha obtenido más ganancias hasta
+la fecha y se le inyecta el 10% de su saldo actual.
+● El equipo de fidelización del banco puede identificar a los usuarios de cada grupo de
+ahorro que más aportan al grupo. A estos usuarios, el banco los premia con un 1%
+menos de comisión a la hora de hacer préstamos.
